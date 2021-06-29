349e70e8ade86164ba5ff62f9983b504f3d6a68f
Ryan Murphy <rdmurphy@users.noreply.github.com>
2021-06-28 21:45:22 -0700

    Few more tweaks

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,7 @@
-const EMBED_SIZE = 'embed-size';
-const INITIAL_MESSAGE = 'frames-init';
-const AMP_SENTINEL = 'amp';
-const FRAME_PREFIX = 'data-frame-';
-const FRAME_AUTO_INITIALIZED = `${FRAME_PREFIX}auto-initialized`;
-const FRAME_SRC = `${FRAME_PREFIX}src`;
-const FRAME_ATTRIBUTE_PREFIX = `${FRAME_PREFIX}attribute-`;
-
-export {
-	AMP_SENTINEL,
-	EMBED_SIZE,
-	FRAME_ATTRIBUTE_PREFIX,
-	FRAME_AUTO_INITIALIZED,
-	FRAME_PREFIX,
-	FRAME_SRC,
-	INITIAL_MESSAGE,
-};
+export const EMBED_SIZE = 'embed-size';
+export const INITIAL_MESSAGE = 'frames-init';
+export const AMP_SENTINEL = 'amp';
+export const FRAME_PREFIX = 'data-frame-';
+export const FRAME_AUTO_INITIALIZED = `${FRAME_PREFIX}auto-initialized`;
+export const FRAME_SRC = `${FRAME_PREFIX}src`;
+export const FRAME_ATTRIBUTE_PREFIX = `${FRAME_PREFIX}attribute-`;
